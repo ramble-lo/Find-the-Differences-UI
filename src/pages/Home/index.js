ffb78e2499f179611a1cf8bd22f6dfae7dd336a8
羅士欽 <s490607@gmail.com>
2021-06-01 02:08:59 +0800

    Add GA

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,8 +4,12 @@ import './index.scss';
 import Result from './components/result/index';
 import Test from './components/test/index';
 
+import ReactGA from 'react-ga';
+import KEY from '../../config.json'
 
 const Home = () => {
+    ReactGA.initialize('追蹤ID');
+    ReactGA.pageview(window.location.pathname + window.location.search);
     const [isFormalFinished, setIsFormalFinished] = useState(false)
     return (
         <div className="container-outside">
