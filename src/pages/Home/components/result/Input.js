59169980f8f00120067cba3af006f4f62773d119
羅士欽 <s490607@gmail.com>
2021-08-15 00:16:40 +0800

    fix:Firebase file naming

--- src/pages/Home/components/result/Input.js
+++ src/pages/Home/components/result/Input.js
@@ -1,6 +1,6 @@
 import React,{useState,useEffect,useRef} from 'react'
 import {useSelector} from 'react-redux'
-import {firebase} from "../../../../ininFirebase";
+import {firebase} from "../../../../initFirebase";
 
 const Input = ({setIsInpitFinished}) => {
     const [playerName, setPlayerName] = useState("");
